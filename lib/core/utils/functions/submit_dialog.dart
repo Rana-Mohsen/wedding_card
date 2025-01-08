35698052fbb10708cb80bfaa6ac4f31f9280917d
Rana-3r2m <rana3r2m@gmail.com>
2025-01-08 02:18:59 +0200

    dialog buttons

--- lib/core/utils/functions/submit_dialog.dart
+++ lib/core/utils/functions/submit_dialog.dart
@@ -14,16 +14,35 @@ void submitDialog(
             onPressed: () {
               GoRouter.of(context).pop();
             },
+            style: buttonStyle(
+              textColor: Colors.white,
+              backgroundColor: Colors.grey,
+            ),
             child: const Text("Edit Data"),
           ),
           TextButton(
             onPressed: () {
               GoRouter.of(context).pop();
             },
+            style: buttonStyle(
+              textColor: Colors.white,
+              backgroundColor: Colors.pinkAccent,
+            ),
             child: const Text("Save"),
           ),
         ],
       );
     },
+  );
+}
+
+ButtonStyle buttonStyle(
+    {required Color? textColor, required Color? backgroundColor}) {
+  return ButtonStyle(
+    foregroundColor: MaterialStateProperty.all(textColor),
+    backgroundColor: MaterialStatePropertyAll(backgroundColor),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+    ),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wedding_card/core/services/firebase_service.dart';
 import 'package:wedding_card/core/utils/routes.dart';
 import 'package:wedding_card/core/utils/service_locator.dart';
-
+import 'package:wedding_card/features/home/data/repos/card_repo_impl.dart';
+import 'features/home/presentation/view_model/card_data_cubit/card_data_cubit.dart';
 
 void main() async {
   //WidgetsFlutterBinding.ensureInitialized();
@@ -18,15 +20,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.routes,
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          // fontFamily: "Montserrat",
-          //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          //useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => CardDataCubit(getIt.get<CardRepoImpl>()),
           ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRoutes.routes,
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            // fontFamily: "Montserrat",
+            //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            //useMaterial3: true,
+            ),
+      ),
     );
   }
 }
