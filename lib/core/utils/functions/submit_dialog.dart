ce41cc944cb01438b58ce9ca577ef645719b681f
Rana-3r2m <rana3r2m@gmail.com>
2025-01-10 03:45:02 +0200

    save dialog state

--- lib/core/utils/functions/submit_dialog.dart
+++ lib/core/utils/functions/submit_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wedding_card/core/utils/handlers/confitti_handler.dart';
 import 'package:wedding_card/core/utils/media_quairy_extention.dart';
@@ -6,49 +7,63 @@ import 'package:wedding_card/features/home/presentation/view_model/card_data_cub
 
 void submitDialog(
     {required BuildContext context, required CardDataCubit submitCard}) {
-  final confettiHandler = ConfettiHandler(duration: const Duration(seconds: 2));
+  final confettiHandler = ConfettiHandler(duration: const Duration(seconds: 1));
 
+  Color? saveBG = Colors.pinkAccent;
+  Color? backBG = Colors.grey;
+  bool excute = true;
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text("Sure you want to save card?"),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              GoRouter.of(context).pop();
-            },
-            style: buttonStyle(
-              textColor: Colors.white,
-              backgroundColor: Colors.grey,
+      return BlocConsumer<CardDataCubit, CardDataState>(
+          listener: (context, state) {
+        if (state is CardDataSubmited) {
+          saveBG = Colors.grey;
+          backBG = Colors.pinkAccent;
+           excute = false;
+        }
+      }, builder: (context, state) {
+        return AlertDialog(
+          title: const Text("Sure you want to save card?"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).pop();
+              },
+              style: buttonStyle(
+                textColor: Colors.white,
+                backgroundColor: backBG,
+              ),
+              child: const Text("Edit Data"),
             ),
-            child: const Text("Edit Data"),
-          ),
-          SizedBox(
-             height: context.screenHeight * 0.055,
-            width: context.screenWidth * 0.12,
-            child: Stack(
-              alignment: AlignmentDirectional.center,
-              fit: StackFit.expand,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    submitCard.submitData();
-                    confettiHandler.play();
-                    //GoRouter.of(context).pop();
-                  },
-                  style: buttonStyle(
-                    textColor: Colors.white,
-                    backgroundColor: Colors.pinkAccent,
+            SizedBox(
+              height: context.screenHeight * 0.055,
+              width: context.screenWidth * 0.12,
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                fit: StackFit.expand,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      if (excute) {
+                        submitCard.submitData();
+                        confettiHandler.play();
+                      }
+                      //GoRouter.of(context).pop();
+                    },
+                    style: buttonStyle(
+                      textColor: Colors.white,
+                      backgroundColor: saveBG,
+                    ),
+                    child: const Text("Save"),
                   ),
-                  child: const Text("Save"),
-                ),
-                confettiHandler.buildConfettiWidget(),
-              ],
+                  confettiHandler.buildConfettiWidget(),
+                ],
+              ),
             ),
-          ),
-        ],
-      );
+          ],
+        );
+      });
     },
   ).then((_) {
     confettiHandler.dispose();
