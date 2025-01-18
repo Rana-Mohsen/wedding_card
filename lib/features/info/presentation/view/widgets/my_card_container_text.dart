d8a8174696dc395661d4bd474be013e06351aa0b
Rana-3r2m <rana3r2m@gmail.com>
2025-01-18 07:00:50 +0200

    cards list view container text

--- lib/features/info/presentation/view/widgets/my_card_container_text.dart
+++ lib/features/info/presentation/view/widgets/my_card_container_text.dart
@@ -10,16 +10,40 @@ class MyCardContainerText extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text("Names: ppppp p ppppppp"),
-        Text("Date: ppppp p ppppppp"),
-        Text(
-          "Location: lksdjdsjsklsjdkljskjdklsjdkljskjdlsjk",
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-          style: TextStyle(color: Colors.white, fontSize: 15),
-          // softWrap: false,
+        CustomRichText(
+          label: "Names",
+          data: "jklfj t ldkjfldjfj",
+        ),
+        CustomRichText(
+          label: "Date",
+          data: "23/23/2334",
+        ),
+        CustomRichText(
+          label: "Location",
+          data: "jklfj kjfdkljf ldkjfldjfj",
         ),
       ],
+    );
+  }
+}
+
+class CustomRichText extends StatelessWidget {
+  const CustomRichText({
+    super.key,
+    required this.label,
+    required this.data,
+  });
+  final String label;
+  final String data;
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      overflow: TextOverflow.ellipsis,
+      maxLines: 2,
+      text: TextSpan(style: const TextStyle(fontSize: 15), children: [
+        TextSpan(text: "$label: "),
+        TextSpan(text: data, style: const TextStyle(color: Colors.black45)),
+      ]),
     );
   }
 }
